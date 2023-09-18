5f471b0752e9291771ec236b4b5953f3a1fdf08b
Josh Guyette <joshguyette@yahoo.com>
2023-09-18 14:35:00 -0500

    ws

--- src/stores.test.ts
+++ src/stores.test.ts
@@ -2,37 +2,37 @@ import { act, renderHook } from '@testing-library/react-hooks/dom';
 import { useLocalStore, useStoreState, useGlobalStore } from './stores'; // Adjust the import path
 
 interface TestState {
-    count: number;
+  count: number;
 }
 
 describe('useCreateStore', () => {
-    it('should create a store with initial state', () => {
-        const { result } = renderHook(() => useLocalStore({ count: 0 }));
-        expect(result.current.getState()).toEqual({ count: 0 });
-    });
+  it('should create a store with initial state', () => {
+    const { result } = renderHook(() => useLocalStore({ count: 0 }));
+    expect(result.current.getState()).toEqual({ count: 0 });
+  });
 });
 
 describe('useStore', () => {
-    it('should return current state and a setter function', () => {
-        const { result: store } = renderHook(() => useLocalStore({ count: 0 }));
-        const { result } = renderHook(() => useStoreState(store.current, 'count'));
-        const [state, setState] = result.current;
-        expect(state).toBe(0);
-        act(() => setState(1));
-        expect(store.current.getState().count).toBe(1);
-    });
+  it('should return current state and a setter function', () => {
+    const { result: store } = renderHook(() => useLocalStore({ count: 0 }));
+    const { result } = renderHook(() => useStoreState(store.current, 'count'));
+    const [state, setState] = result.current;
+    expect(state).toBe(0);
+    act(() => setState(1));
+    expect(store.current.getState().count).toBe(1);
+  });
 });
 
 describe('useGlobalStore', () => {
-    it('should create and retrieve a global store', () => {
-        const { result } = renderHook(() => useGlobalStore<TestState>('testNamespace', { count: 0 }));
-        expect(result.current.getState()).toEqual({ count: 0 });
+  it('should create and retrieve a global store', () => {
+    const { result } = renderHook(() => useGlobalStore<TestState>('testNamespace', { count: 0 }));
+    expect(result.current.getState()).toEqual({ count: 0 });
 
-        const { result: store } = renderHook(() => useGlobalStore<TestState>('testNamespace'));
-        const { result: globalStoreResult } = renderHook(() => useStoreState(store.current, 'count'));
-        const [globalState, setGlobalState] = globalStoreResult.current;
-        expect(globalState).toBe(0);
-        act(() => setGlobalState(2));
-        expect(result.current.getState().count).toBe(2);
-    });
+    const { result: store } = renderHook(() => useGlobalStore<TestState>('testNamespace'));
+    const { result: globalStoreResult } = renderHook(() => useStoreState(store.current, 'count'));
+    const [globalState, setGlobalState] = globalStoreResult.current;
+    expect(globalState).toBe(0);
+    act(() => setGlobalState(2));
+    expect(result.current.getState().count).toBe(2);
+  });
 });
